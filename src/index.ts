7295a135e1410ed0e2cd25664b03baf4d07d4a07
dxu <xudavidb@gmail.com>
2023-08-23 11:50:23 -0700

    update exports with useSelectAtom

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { Atom } from "./Atom";
 export { useAtom } from "./useAtom";
+export { useSelectAtom } from "./useSelectAtom";
 export { hydrateAtoms } from "./hydrateAtoms";
 export { Signal } from "./Signal";
