6ce77a8fa57761fb78c408429ca0b6d6c65ffa37
dxu <xudavidb@gmail.com>
2023-11-13 10:57:24 -0800

    add ability to force rehydration

--- src/useHydrateAtoms.ts
+++ src/useHydrateAtoms.ts
@@ -4,10 +4,11 @@ import { BaseAtom, NullableBaseAtom, ReadOnlyAtom } from "./Atom";
 const hydratedAtomsSet: WeakSet<ReadOnlyAtom<any>> = new WeakSet();
 
 export function hydrateAtoms(
-  values: readonly [NullableBaseAtom<any> | BaseAtom<any>, any][]
+  values: readonly [NullableBaseAtom<any> | BaseAtom<any>, any][],
+  options?: { force?: boolean }
 ) {
   for (const [atom, value] of values) {
-    if (!hydratedAtomsSet.has(atom)) {
+    if (!hydratedAtomsSet.has(atom) || options?.force) {
       hydratedAtomsSet.add(atom);
       atom._behavior$.next(value);
     }
