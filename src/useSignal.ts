e16373d1bd3770e479c3f546650170dda29139a2
dxu <xudavidb@gmail.com>
2024-04-05 16:18:58 -0700

    allow optional signal cb

--- src/useSignal.ts
+++ src/useSignal.ts
@@ -1,19 +1,24 @@
 import { useEffect, useState } from "react";
 import { Signal } from "./Signal";
+import { Subscription } from "rxjs";
 
 export function useSignal<T>(
   signal: Signal<T>,
-  callback: (update: T) => void,
+  callback?: (update: T) => void,
   id?: string
 ) {
   useEffect(() => {
     // Assuming the signal might not have an initial value method like `atom.value()`,
     // If your signal class has a method to get the current/latest value, use it here to initialize.
 
-    const subscription = signal.subscribe(callback, id);
-
+    let subscription: Subscription;
+    if (callback) {
+      subscription = signal.subscribe(callback, id);
+    }
     return () => {
-      subscription.unsubscribe();
+      if (subscription) {
+        subscription.unsubscribe();
+      }
     };
   }, [signal]);
 }
